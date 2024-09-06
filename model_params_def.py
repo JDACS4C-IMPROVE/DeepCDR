d3471f9ee5cd73c6dd4f0399a09aa9ea277bbe8d
Gayara-Fernando <gayara-demini@huskers.unl.edu>
2024-09-06 10:59:38 -0700

    add model specific parameters

--- model_params_def.py
+++ model_params_def.py
@@ -1,5 +1,5 @@
 """
-Model-specific params (Model: GraphDRP)
+Model-specific params (Model: DeepCDR)
 If no params are required by the model, then it should be an empty list.
 """
 
@@ -9,7 +9,37 @@ from improvelib.utils import str2bool
 preprocess_params = []
 
 
-train_params = []
+train_params = [
+    {"name": "epochs",
+     "type": int,
+     "default": 150,
+     "help": "Number of epochs for training."
+    },
+
+    {"name": "learning_rate",
+     "type": float,
+     "default": 0.0001,
+     "help": "Learning rate for the optimizer."
+    },
+
+    {"name": "batch_size",
+     "type": int,
+     "default": 256,
+     "help": "Training batch size."
+    },
+
+    {"name": "val_batch",
+     "type": int,
+     "default": 256,
+     "help": "Validation batch size."
+    }
+]
 
 
-infer_params = []
+infer_params = [
+    {"name": "infer_batch",
+     "type": int,
+     "default": 256,
+     "help": "Inference batch size."
+    }
+]
