f5399ca87782efe29575a30d6e7332d497cb7323
Gayara-Fernando <gayara-demini@huskers.unl.edu>
2024-09-12 08:32:57 -0700

    remove improvelib params

--- model_params_def.py
+++ model_params_def.py
@@ -9,44 +9,7 @@ from improvelib.utils import str2bool
 preprocess_params = []
 
 
-train_params = [
-    {"name": "epochs",
-     "type": int,
-     "default": 150,
-     "help": "Number of epochs for training."
-    },
-
-    {"name": "learning_rate",
-     "type": float,
-     "default": 0.0001,
-     "help": "Learning rate for the optimizer."
-    },
-
-    {"name": "batch_size",
-     "type": int,
-     "default": 256,
-     "help": "Training batch size."
-    },
-
-    {"name": "val_batch",
-     "type": int,
-     "default": 256,
-     "help": "Validation batch size."
-    },
-
-    {"name": "patience",
-     "type": int,
-     "default": 20,
-     "help": "No.of epchs to run before exiting out of the training loop"
-    }
-
-]
+train_params = []
 
 
-infer_params = [
-    {"name": "infer_batch",
-     "type": int,
-     "default": 256,
-     "help": "Inference batch size."
-    }
-]
+infer_params = []
